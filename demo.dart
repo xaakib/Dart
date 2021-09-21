227768436dec6f7ffcf570ba80d0b78a4493f540
Xaakib <64243974+xaakib@users.noreply.github.com>
2021-09-21 12:36:49 +0600

    Uploaded Dart File

---
+++ demo.dart
@@ -0,0 +1,8 @@
+class DemoClass {
+  printObject(String name , int age){
+    
+    var equalValue =  name+age.toString();
+    return equalValue;
+  }
+  
+}
---
+++ main.dart
@@ -0,0 +1,7 @@
+import '';
+import 'demo.dart';
+DemoClass demoClass = DemoClass();
+void main(){
+ var printOutput = demoClass.printObject('Ashiq', 25);
+ print(printOutput);
+}

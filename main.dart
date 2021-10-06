63c26ca32d3c042fe978ccf086c520f44160bdca
Xaakib <64243974+xaakib@users.noreply.github.com>
2021-10-06 17:06:59 +0600

    datatype checked

--- main.dart
+++ main.dart
@@ -1,10 +1,14 @@
-
-
 import 'demo.dart';
 
 DemoClass demoClass = DemoClass();
 
 void main() {
-  demoClass.inputUserInfo();
-  print("Thank You ! Goodbye ;)");
+  // demoClass.inputUserInfo();
+  // print("Thank You ! Goodbye ;)");
+  getTypeConversion("31");
+}
+
+getTypeConversion(var data) {
+  var dataType = data;
+  print(dataType.runtimeType.toString());
 }

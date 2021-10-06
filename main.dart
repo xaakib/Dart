7843feb8c4fb9af5455ded78efbf0ba7ed2cbc01
Xaakib <64243974+xaakib@users.noreply.github.com>
2021-10-06 10:42:05 +0600

    changed

--- main.dart
+++ main.dart
@@ -1,13 +1,10 @@
-import 'dart:io';
+
+
 import 'demo.dart';
+
 DemoClass demoClass = DemoClass();
 
-
 void main() {
-  // String? name = stdin.readLineSync();
-  // int? age = int.parse(stdin.readLineSync()!);
-  // var printOutput = demoClass.printObject(name!, age);
-  // print("printed Output : $printOutput");
-  print(demoClass.listOfMapFind());
-
+  demoClass.inputUserInfo();
+  print("Thank You ! Goodbye ;)");
 }

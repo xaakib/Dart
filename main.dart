921e1e095d65b0e35bf631301f61e384a08e1d1b
Xaakib <64243974+xaakib@users.noreply.github.com>
2021-10-10 19:05:30 +0600

    finded list to index and map item

--- main.dart
+++ main.dart
@@ -1,27 +1,57 @@
-import 'demo.dart';
-import 'total_price.dart';
+// import 'demo.dart';
+// import 'total_price.dart';
 
-DemoClass demoClass = DemoClass();
+// DemoClass demoClass = DemoClass();
+
+// void main() {
+//   // TotalPriceClass totalPriceClass = TotalPriceClass();
+//   // print(totalPriceClass.loop());
+
+//   List list = [
+//     {"price": 20},
+//     {"price": 38},
+//     {"price": 24},
+//   ];
+
+//   for (var i = 0; i < list.length; i++) {
+//     print(i);
+  
+//   }
+//   // demoClass.inputUserInfo();
+//   // print("Thank You ! Goodbye ;)");
+//   // getTypeConversion("31");
+//   // getMap();
+//   // getMap2();
+//   // print("ClassList");
+//   // print(listClass.li);
+// }
+
+
+List movieData = [
+  {"id": 1, "title": "August 1975", "mediaid": 2, "price": 10, "discount": 5, "imageurl": "https://mgrdev.cinebaz.com/storage/dropzon/2021-07/small/augost751020x1560_1626336629.jpg", "createdAt": "2021-10-10 09:29:37"},
+   {"id": 1, "title": "name add", "mediaid": 2, "price": 20, "discount": 5, "imageurl": "https://mgrdev.cinebaz.com/storage/dropzon/2021-07/small/augost751020x1560_1626336629.jpg", "createdAt": "2021-10-10 09:29:37"},
+ {"id": 1, "title": "name 2", "mediaid": 2, "price": 20, "discount": 5, "imageurl": "https://mgrdev.cinebaz.com/storage/dropzon/2021-07/small/augost751020x1560_1626336629.jpg", "createdAt": "2021-10-10 09:29:37"},
+
+];
+
 
 void main() {
-  // TotalPriceClass totalPriceClass = TotalPriceClass();
-  // print(totalPriceClass.loop());
-
-  List list = [
-    {"price": 20},
-    {"price": 38},
-    {"price": 24},
-  ];
-
-  for (var i = 0; i < list.length; i++) {
-    print(i);
-  
+  List li =[];
+  for (var i in movieData){
+    
+    Map<String, dynamic> map = {
+  'name': i['title'],
+      'price': i['price'],
+};
+    li.add(map);
   }
-  // demoClass.inputUserInfo();
-  // print("Thank You ! Goodbye ;)");
-  // getTypeConversion("31");
-  // getMap();
-  // getMap2();
-  // print("ClassList");
-  // print(listClass.li);
+  
+ print(li);
+//   li.removeAt(1);
+   li.removeAt(0);
+  print(li);
+  
+  
+  
+ 
 }

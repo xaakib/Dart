6a1d3822a65bcc56e283935aa12eab9906bc080f
Xaakib <64243974+xaakib@users.noreply.github.com>
2021-10-12 16:59:07 +0600

    add list and sort

--- main.dart
+++ main.dart
@@ -25,34 +25,46 @@
 //   // print("ClassList");
 //   // print(listClass.li);
 // }
-
-
-List movieData = [
-  {"id": 1, "title": "August 1975", "mediaid": 2, "price": 10, "discount": 5, "imageurl": "https://mgrdev.cinebaz.com/storage/dropzon/2021-07/small/augost751020x1560_1626336629.jpg", "createdAt": "2021-10-10 09:29:37"},
-   {"id": 1, "title": "name add", "mediaid": 2, "price": 20, "discount": 5, "imageurl": "https://mgrdev.cinebaz.com/storage/dropzon/2021-07/small/augost751020x1560_1626336629.jpg", "createdAt": "2021-10-10 09:29:37"},
- {"id": 1, "title": "name 2", "mediaid": 2, "price": 20, "discount": 5, "imageurl": "https://mgrdev.cinebaz.com/storage/dropzon/2021-07/small/augost751020x1560_1626336629.jpg", "createdAt": "2021-10-10 09:29:37"},
-
-];
-
 void main() {
-  var x = 4;
-  
-  fun(){
-    var x = "no";
-    print("This is the $x");
-  }
-   pri(){
-     var t = 2;
-       fun();
-     return t;
+    List li = [];
+  // Creating a string
+  String name = "s h a k i b";
     
-  }
+  // Splitting each
+  // character of the string
+var output = name.split(' ');
+ li = output;
+ li.sort();
+  print(li);
+
+}
+
+// List movieData = [
+//   {"id": 1, "title": "August 1975", "mediaid": 2, "price": 10, "discount": 5, "imageurl": "https://mgrdev.cinebaz.com/storage/dropzon/2021-07/small/augost751020x1560_1626336629.jpg", "createdAt": "2021-10-10 09:29:37"},
+//    {"id": 1, "title": "name add", "mediaid": 2, "price": 20, "discount": 5, "imageurl": "https://mgrdev.cinebaz.com/storage/dropzon/2021-07/small/augost751020x1560_1626336629.jpg", "createdAt": "2021-10-10 09:29:37"},
+//  {"id": 1, "title": "name 2", "mediaid": 2, "price": 20, "discount": 5, "imageurl": "https://mgrdev.cinebaz.com/storage/dropzon/2021-07/small/augost751020x1560_1626336629.jpg", "createdAt": "2021-10-10 09:29:37"},
+
+// ];
+
+// void main() {
+//   var x = 4;
+  
+//   fun(){
+//     var x = "no";
+//     print("This is the $x");
+//   }
+//    pri(){
+//      var t = 2;
+//        fun();
+//      return t;
+    
+//   }
  
-  print("last$x");
-  print(pri());
+//   print("last$x");
+//   print(pri());
   
     
-  }
+//   }
 
 
 // void main() {
